9d1bcb631a06384a0f067a235f9b5dded34be7d3
18127003 <18127003@student.hcmus.edu.vn>
2025-03-19 12:59:30 -0230

    add recursive merkle tree

--- src/main/java/com/cloud/merkle/Helper.java
+++ src/main/java/com/cloud/merkle/Helper.java
@@ -3,44 +3,50 @@ package com.cloud.merkle;
 import com.google.cloud.functions.HttpFunction;
 import com.google.cloud.functions.HttpRequest;
 import com.google.cloud.functions.HttpResponse;
-import com.google.cloud.pubsub.v1.Publisher;
 import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
-import com.google.protobuf.ByteString;
-import com.google.pubsub.v1.ProjectTopicName;
-import com.google.pubsub.v1.PubsubMessage;
 
 import java.io.File;
-import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.List;
 
 public class Helper implements HttpFunction {
+    public static long measureRecursiveMerkleTree(byte[][] data) throws NoSuchAlgorithmException {
+        Instant start = Instant.now();
+        byte[] root = RecursiveMerkleTree.generateMerkleRoot(data);
+        Instant end = Instant.now();
+        return Duration.between(start,end).toMillis();
+    }
+
+    public static long measureParallelMerkleTree(byte[][] data) throws NoSuchAlgorithmException {
+        Instant start = Instant.now();
+        byte[] root = ParallelMerkleTree.generateMerkleRoot(data);
+        Instant end = Instant.now();
+        return Duration.between(start,end).toMillis();
+    }
+
+    public static long measureStandardMerkleTree(byte[][] data) throws NoSuchAlgorithmException {
+        Instant start = Instant.now();
+        byte[] root = MerkleTree.generateMerkleRoot(data);
+        Instant end = Instant.now();
+        return Duration.between(start,end).toMillis();
+    }
 
     /***
      * Parse data into uniform chunks (ideally each row is a chunk)
      */
-    public static byte[][] parseData(String fileName, int chunkSize) throws IOException {
-        File file = new File(fileName);
-        try (FileInputStream fis = new FileInputStream(file)) {
-            byte[][] dataBytes = new byte[(int) (file.length() / chunkSize)][chunkSize];
-            for (int i = 0; i < dataBytes.length; i++) {
-                fis.read(dataBytes[i], 0, chunkSize);
-            }
-            fis.close();
-            return dataBytes;
+    public static byte[][] parseData(byte[] input, int chunkSize) {
+        byte[][] dataBytes = new byte[input.length/ chunkSize][chunkSize];
+        for (int i = 0; i < dataBytes.length; i++) {
+            System.arraycopy(input, i * chunkSize, dataBytes[i], 0, chunkSize);
         }
-    }
-
-    public static byte[] strToBytes(String str) {
-        return str.getBytes(StandardCharsets.UTF_8);
+        return dataBytes;
     }
 
     public static String encodeHexString(byte[] byteArray) {
@@ -66,37 +72,14 @@ public class Helper implements HttpFunction {
             httpResponse.getWriter().write("File not found.");
             return;
         }
-        String[] params = httpRequest.getReader().readLine().split(",");
-        int offset = Integer.parseInt(params[0]);
-        int nodes = Integer.parseInt(params[1]);
-        int chunkSize = Integer.parseInt(params[2]);
 
-        byte[][] data = new byte[nodes][chunkSize];
         byte[] raw = blob.getContent();
-
-        for (int i = 0; i < nodes; i++) {
-            System.arraycopy(raw, offset + i*chunkSize, data[i], 0, chunkSize);
-        }
+        byte[][] data = parseData(raw, 64);
 
         try {
-            httpResponse.getWriter().write("Standard");
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
-            Instant start = Instant.now();
-            MerkleTree.generateMerkleTree(data, md);
-            Instant end = Instant.now();
-            String time = String.valueOf(Duration.between(start,end).toMillis());
-            // Publish
-            ProjectTopicName topicName = ProjectTopicName.of("protean-music-381914", "merkle");
-            Publisher publisher = Publisher.newBuilder(topicName).build();
-
-            PubsubMessage pubsubMessage = PubsubMessage.newBuilder()
-                    .setData(ByteString.copyFromUtf8(time))
-                    .build();
-
-            // Publish the message to the topic
-            publisher.publish(pubsubMessage).get();
-            // Shutdown the publisher (important for cleanup)
-            publisher.shutdown();
+            httpResponse.getWriter().write(String.format("Standard: %d\n", measureStandardMerkleTree(data)));
+            httpResponse.getWriter().write(String.format("Parallel: %d\n", measureParallelMerkleTree(data)));
+            httpResponse.getWriter().write(String.format("Recursive: %d\n", measureRecursiveMerkleTree(data)));
         }
         catch (NoSuchAlgorithmException ae) {
             httpResponse.getWriter().write("SHA-256 not supported");
